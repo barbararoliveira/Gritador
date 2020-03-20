bbcea3038154b2b099e017ac09621c129b71be20
Bárbara <babyr.oliveira@gmail.com>
2020-03-20 15:11:52 -0300

    Resolução do exercício proposto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, digite o seu nome:");
+            string nome = Console.ReadLine();
+             Console.WriteLine (nome.ToUpper());
+        }
+    }
+}
